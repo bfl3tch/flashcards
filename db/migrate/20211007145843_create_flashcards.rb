ae0d6cc7ece6f9a66428dbb7cf2fa36644a7be67
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-07 21:10:10 -0400

    Schema updated for flashcard removal of dependencies

--- db/migrate/20211007145843_create_flashcards.rb
+++ db/migrate/20211007145843_create_flashcards.rb
@@ -3,9 +3,9 @@ class CreateFlashcards < ActiveRecord::Migration[5.2]
     create_table :flashcards do |t|
       t.string :question
       t.string :answer
+      t.string :category
       t.integer :correct_guesses, default: 0
       t.integer :incorrect_guesses, default: 0
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
