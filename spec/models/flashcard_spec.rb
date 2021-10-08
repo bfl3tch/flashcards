d950030e90dde86c223b001ac6d4d0544e82f514
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-07 21:08:34 -0400

    Flashcard model specs

--- spec/models/flashcard_spec.rb
+++ spec/models/flashcard_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Flashcard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:question) }
+    it { should validate_presence_of(:answer) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:correct_guesses) }
+    it { should validate_presence_of(:incorrect_guesses) }
+  end
 end
