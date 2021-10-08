599ec2af14e25d6008ad09bf08ef35b4b90d8655
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-07 21:10:22 -0400

    Routes modified for removing dependency of user off flashcard

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,8 @@ Rails.application.routes.draw do
   get '/auth/github', to: 'authentication#github', format: false
   namespace :api do
     namespace :v1 do
-
-      resources :tags do
-        get :flashcards, on: :member
-      end
-
-      resources :flashcards do
-        get :tags, on: :member
-      end
-
       resources :users
+      resources :flashcards
     end
   end
 end
