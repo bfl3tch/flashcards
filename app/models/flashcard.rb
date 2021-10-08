70aca8619201716fa704c28b8a7009ffefc65a02
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-10-07 21:09:43 -0400

    Flashcard model validations modified

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,7 +1,3 @@
 class Flashcard < ApplicationRecord
-  belongs_to :user
-  has_many :taggings, dependent: :destroy
-  has_many :tags, through: :taggings
-
-  validates_presence_of :question, :answer, :correct_guesses, :incorrect_guesses
+  validates_presence_of :question, :answer, :category, :correct_guesses, :incorrect_guesses
 end
